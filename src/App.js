e6587bc2059023ce688f0c79a5c016b538c5fe82
torranicles <torranicles@gmail.com>
2021-04-11 01:09:52 +0800

    Move Home.js states to App.js

--- src/App.js
+++ src/App.js
@@ -2,19 +2,87 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Issues from './components/Issues'
 import Home from './components/Home'
+import axios from 'axios'
 
 class App extends React.Component {
-  constructor(props) {
-      super(props);
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            search: '',
+            message: '',
+            viewForm: false
+        }
+        this.handleSearchChange = this.handleSearchChange.bind(this);
+        this.handleFormChange = this.handleFormChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+        this.handleViewForm = this.handleViewForm.bind(this);
+        this.handleCloseForm = this.handleCloseForm.bind(this);
+    }
+    handleViewForm() {
+        this.setState({
+            viewForm: true
+        })
+    }
+
+    handleCloseForm() {
+        this.setState({
+            viewForm: false
+        })
+    }
+
+    handleSearchChange(e) {
+        this.setState({
+            search: e.target.value
+        })
+    }
+
+    handleSearch(e) {
+        e.preventDefault();
+        axios.get('/search', {
+            params: {
+                project: this.state.search
+            }
+        })
+        .then(res => {
+            if (res.data.length < 1) {
+                this.setState({
+                    message: 'Project name does not exist.'
+                })
+            } else {
+                console.log(res);
+                this.props.history.push(`/projects-issues/${this.state.search}`);
+            }
+        })
+    }
+
+    handleFormChange(e) {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        axios.post('/api/issues/add', this.state)
+            .then(res => console.log(res))
     }
     render() {
-        
+        console.log(this.state)
         return (
             <Router>
                 <Switch>
                     <Route exact path="/" render={props => {
                             return <Home 
-                                {...props}
+                                {...props} 
+                                handleSearch={this.handleSearch}
+                                handleSearchChange={this.handleSearchChange}
+                                handleViewForm={this.handleViewForm}
+                                handleCloseForm={this.handleCloseForm}
+                                handleFormChange={this.handleFormChange}
+                                handleSubmit={this.handleSubmit}
+                                viewForm={this.state.viewForm}
                             />} 
                         }/>
                     <Route exact path="/projects-issues/:project" component={Issues}/>
