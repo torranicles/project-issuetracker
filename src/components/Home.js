bf546f96cd76798df0520e0ee92e0712d7abf55e
torranicles <torranicles@gmail.com>
2021-04-11 01:09:36 +0800

    Change to functional component/move states to App.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,84 +1,17 @@
 import React from 'react'
-import axios from 'axios'
 import styles from '../Home.module.css'
 import AddIssue from './AddIssue.js'
 
-class Home extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            search: '',
-            message: '',
-            viewForm: false
-        }
-        this.handleSearchChange = this.handleSearchChange.bind(this);
-        this.handleFormChange = this.handleFormChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleSearch = this.handleSearch.bind(this);
-        this.handleViewForm = this.handleViewForm.bind(this);
-        this.handleCloseForm = this.handleCloseForm.bind(this);
-    }
-    handleViewForm() {
-        this.setState({
-            viewForm: true
-        })
-    }
-
-    handleCloseForm() {
-        this.setState({
-            viewForm: false
-        })
-    }
-
-    handleSearchChange(e) {
-        this.setState({
-            search: e.target.value
-        })
-    }
-
-    handleSearch(e) {
-        e.preventDefault();
-        axios.get('/search', {
-            params: {
-                project: this.state.search
-            }
-        })
-        .then(res => {
-            if (res.data.length < 1) {
-                this.setState({
-                    message: 'Project name does not exist.'
-                })
-            } else {
-                console.log(res);
-                this.props.history.push(`/projects-issues/${this.state.search}`);
-            }
-        })
-    }
-
-    handleFormChange(e) {
-        this.setState({
-            [e.target.name] : e.target.value
-        })
-    }
-
-    handleSubmit(e) {
-        e.preventDefault();
-        axios.post('/api/issues/add', this.state)
-            .then(res => console.log(res))
-    }
-
-    render() {
-        console.log(this.state)
-        return (
-            <div className={styles.bg}>
+const Home = (props) => {
+    return (
+        <div className={styles.bg}>
                 <div className="w-50 h-100">
                     <h1 className={styles.head}>
                         issue tracker
                     </h1>
-                    <form className={`${styles.searchContainer} shadow`} onSubmit={this.handleSearch}>
+                    <form className={`${styles.searchContainer} shadow`} onSubmit={props.handleSearch}>
                         <input type="text" 
-                            onChange={this.handleSearchChange} 
+                            onChange={props.handleSearchChange} 
                             placeholder="Project name..."
                             className="form-control"
                             style={{
@@ -88,23 +21,25 @@ class Home extends React.Component {
                         />
                         <input className="btn btn-primary px-5" type="submit" value="Search"/>
                     </form>
-                    <div className={styles.addBtn} onClick={this.handleViewForm}>
+                    <div className={styles.addBtn} onClick={props.handleViewForm}>
                         <i className="fas fa-folder-plus mr-2 text-primary"/><span>Start a new issue</span>
                     </div>
                 </div>
                 {
-                    this.state.viewForm 
+                    props.viewForm 
                     ? <div className="d-flex align-items-center justify-content-center w-50 h-100">
                         <div className={styles.formContainer}>
-                            <AddIssue handleChange={this.handleFormChange} handleSubmit={this.handleSubmit} handleCloseForm={this.handleCloseForm}/>
+                            <AddIssue 
+                                handleChange={props.handleFormChange} 
+                                handleSubmit={props.handleSubmit} 
+                                handleCloseForm={props.handleCloseForm}/>
                         </div>
                     </div>
                     : null
                 }
                 
             </div>
-        )
-    }
+    )
 }
 
 export default Home;
