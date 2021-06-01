75c182de72acf93f394f8ec17d971340d3676850
torranicles <torranicles@gmail.com>
2021-06-01 19:11:18 +0800

    Add style on active page button

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -7,11 +7,22 @@ const Pagination = ({ issuesPerPage, totalIssues, paginate }) => {
         pageNumbers.push(i)
     }
 
+    let buttons = document.querySelectorAll('.page-item');
+    
+    if (buttons) {
+        buttons.forEach(button => {
+            button.addEventListener('click', function() {
+                buttons.forEach(btn => btn.classList.remove('active'));
+                this.classList.add('active');
+            })
+        })
+    }
+
     return (
         <ul className="pagination">
             {
                 pageNumbers.map(num => (
-                    <li key={num} className="page-item">
+                    <li key={num} className={`page-item ${num === 1 ? 'active' : ''}`}>
                         <a onClick={() => paginate(num)} href="#!" className="page-link">
                             {num}
                         </a>
